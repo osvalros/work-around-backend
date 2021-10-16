7e42f13976739ef798600530b559c67429ede2e3
Filip Sikora <filip.sikora0123@gmail.com>
2021-10-16 15:56:49 +0200

    add property-rentals model

--- graphql_api/models.py
+++ graphql_api/models.py
@@ -9,3 +9,10 @@ class User(AbstractUser):
 class Property(models.Model):
     coordinates = models.PointField(geography=True)
     user = models.ForeignKey(User, models.CASCADE)
+    is_available = models.BooleanField(default=False)
+    usd_worth = models.FloatField()
+
+
+class PropertyRentals(models.Model):
+    user = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    property = models.ForeignKey(Property, models.SET_NULL, blank=True, null=True)
