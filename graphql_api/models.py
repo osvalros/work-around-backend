f11fc20811eec3a7a9b6db325642e5268c6f6b61
Rostislav Osvald <rostislav.osvald@gmail.com>
2021-10-16 15:32:51 +0200

    Add property model

--- graphql_api/models.py
+++ graphql_api/models.py
@@ -1,6 +1,11 @@
-from django.db import models
+from django.contrib.gis.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+
+
+class Property(models.Model):
+    coordinates = models.PointField()
+    user = models.ForeignKey(User, models.CASCADE)
