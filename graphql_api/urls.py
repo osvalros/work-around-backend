cceee3ac9155681b7cf41708393482647d37d37b
Rostislav Osvald <rostislav.osvald@gmail.com>
2021-10-17 03:30:09 +0200

    Add logging of exceptions

--- graphql_api/urls.py
+++ graphql_api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
 from graphql_jwt.decorators import jwt_cookie
 from graphql_playground.views import GraphQLPlaygroundView
 
+from graphql_api.views import WorkAroundGraphQLView
 from work_around.settings import DEBUG
 
 urlpatterns = [
-    path("", csrf_exempt(jwt_cookie(GraphQLView.as_view(graphiql=True)))),
+    path("", csrf_exempt(jwt_cookie(WorkAroundGraphQLView.as_view(graphiql=True)))),
 ]
 
 if DEBUG:
--- graphql_api/views.py
+++ graphql_api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from graphene_django.views import GraphQLView
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_CONTEXT_VARIABLES = {}
+
+
+def set_default_request_context_variables(request):
+    for k, v in DEFAULT_CONTEXT_VARIABLES.items():
+        setattr(request, k, v)
+
+
+class WorkAroundGraphQLView(GraphQLView):
+    def execute_graphql_request(self, request, *args, **kwargs):
+        set_default_request_context_variables(request)
+        result = super().execute_graphql_request(request, *args, **kwargs)
+        if result.errors:
+            for error in result.errors:
+                try:
+                    raise error.original_error
+                except Exception as e:
+                    logger.exception(e)
+        return result
